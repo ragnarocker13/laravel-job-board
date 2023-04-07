ff816a0bbfae18e056c6c6e3f74b03e7e0875aa4
ragnarocker13 <112006063+ragnarocker13@users.noreply.github.com>
2023-04-07 22:33:04 +0800

    Fixed clickable category tag and page

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -9,7 +9,7 @@
     <li
     class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
     >
-    <a href="/?tag={{ $tag }}">{{ $tag }}</a>
+    <a href="/listings/?tag={{$tag}}">{{ $tag }}</a>
     </li>
     @endforeach
 
